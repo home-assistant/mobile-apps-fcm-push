2060e419c63d20188c96ec05fe9d40d038eecf24
Basset, Justin <Bassett.JustinT@gmail.com>
2020-02-04 16:19:24 -0500

    Extract ios logic into own JS file.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # mobile-apps-fcm-push
 
 This is the code that powers the official mobile app push notifications. Feel free to submit PRs!
+
+
+## Developer Setup
+
+Install NPM dependencies:
+```
+cd functions
+npm install
+```
+
+Startup function for local testing:
+```
+# Only Once
+npm install -g firebase-tools
+
+# Whenever you want to start
+npm run serve
+```
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ios = require('./ios')
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -20,141 +22,24 @@ exports.sendPushNotification = functions.https.onRequest(async (req, res) => {
   }
   var ref = db.collection('rateLimits').doc(today).collection('tokens').doc(token);
 
-  var payload = {
-    notification: {
-      body: req.body.message,
-    },
-    apns: {
-      headers: {},
-      payload: {
-        aps: {
-          alert: {
-            body: req.body.message
-          },
-          sound: 'default'
-        }
-      }
-    },
-    token: token,
-  };
-
-  if(req.body.title) {
-    payload.notification.title = req.body.title;
-    payload.apns.payload.aps.alert.title = req.body.title;
-  }
-
-  if(req.body.data) {
-    if(req.body.data.android) {
-      payload.android = req.body.data.android;
-    }
-    if(req.body.data.apns) {
-      payload.apns = req.body.data.apns;
-    }
-    if(req.body.data.apns_headers) {
-      payload.apns.headers = req.body.data.apns_headers;
-    }
-    if(req.body.data.data) {
-      payload.data = req.body.data.data;
-    }
-    if(req.body.data.webpush) {
-      payload.webpush = req.body.data.webpush;
-    }
-  }
-
-  var updateRateLimits = true;
-
+  var updateRateLimits = true
+  var payload = null
   if(req.body.registration_info.app_id.indexOf('io.robbie.HomeAssistant') > -1) {
-    // Enable old SNS iOS specific push setup.
-    if (req.body.message === 'request_location_update' || req.body.message === 'request_location_updates') {
-      payload.notification = {};
-      payload.apns.payload.aps = {};
-      payload.apns.payload.aps.contentAvailable = true;
-      payload.apns.payload.homeassistant = { 'command': 'request_location_update' };
-      updateRateLimits = false;
-    } else if (req.body.message === 'clear_badge') {
-      payload.notification = {};
-      payload.apns.payload.aps = {};
-      payload.apns.payload.aps.contentAvailable = true;
-      payload.apns.payload.aps.badge = 0;
-      payload.apns.payload.homeassistant = { 'command': 'clear_badge' };
-      updateRateLimits = false;
-    } else {
-      if(req.body.data) {
-        if (req.body.data.subtitle) {
-          payload.apns.payload.aps.alert.subtitle = req.body.data.subtitle;
-        }
-
-        if (req.body.data.push) {
-          for (var attrname in req.body.data.push) {
-            payload.apns.payload.aps[attrname] = req.body.data.push[attrname];
-          }
-        }
-
-        if(req.body.data.sound) {
-          payload.apns.payload.aps.sound = req.body.data.sound;
-        } else if(req.body.data.push && req.body.data.push.sound) {
-          payload.apns.payload.aps.sound = req.body.data.push.sound;
-        }
-
-        if (req.body.data.entity_id) {
-          payload.apns.payload.entity_id = req.body.data.entity_id;
-        }
-
-        if (req.body.data.action_data) {
-          payload.apns.payload.homeassistant = req.body.data.action_data;
-        }
-
-        if (req.body.data.attachment) {
-          payload.apns.payload.attachment = req.body.data.attachment;
-        }
-
-        if (req.body.data.url) {
-          payload.apns.payload.url = req.body.data.url;
-        }
-
-        if (req.body.data.shortcut) {
-          payload.apns.payload.shortcut = req.body.data.shortcut;
-        }
-
-        if (req.body.data.presentation_options) {
-          payload.apns.payload.presentation_options = req.body.data.presentation_options;
-        }
-      }
-
-      payload.apns.payload.aps.mutableContent = true;
-
-      if (req.body.message === 'delete_alert') {
-        updateRateLimits = false;
-        delete payload.notification.body;
-        delete payload.apns.payload.aps.alert.title;
-        delete payload.apns.payload.aps.alert.subtitle;
-        delete payload.apns.payload.aps.alert.body;
-        delete payload.apns.payload.aps.sound;
-      }
-    }
-  }
-
-  if(payload.apns.payload.aps.sound) {
-    if((typeof payload.apns.payload.aps.sound === "string") && (payload.apns.payload.aps.sound.toLowerCase() === "none")) {
-      delete payload.apns.payload.aps.sound;
-    } else if(typeof payload.apns.payload.aps.sound === "object") {
-      if(payload.apns.payload.aps.sound.volume) {
-        payload.apns.payload.aps.sound.volume = parseFloat(payload.apns.payload.aps.sound.volume);
-      }
-      if(payload.apns.payload.aps.sound.critical) {
-        payload.apns.payload.aps.sound.critical = parseInt(payload.apns.payload.aps.sound.critical);
-      }
-      if(payload.apns.payload.aps.sound.critical && payload.apns.payload.aps.sound.volume > 0) {
-        updateRateLimits = false;
-      }
-    }
-  }
-  if(payload.apns.payload.aps.badge) payload.apns.payload.aps.badge = Number(payload.apns.payload.aps.badge);
-  if(payload.apns.payload.aps.contentAvailable) {
-    payload.apns.headers['apns-push-type'] = 'background';
+    var response = ios.createPayload(req)
+    updateRateLimits = response[0]
+    payload = response[1]
+  } else if (req.body.registration_info.app_id.indexOf('io.homeassistant.companion.android') > -1) {
+    // TODO: Android Handling
   } else {
-    payload.apns.headers['apns-push-type'] = 'alert';
+    console.error('Issue deternmining android vs ios')
+    return res.status(400).send({
+      errorType: 'MissingRegistration',
+      errorStep: 'RegistrationCheck'
+    });
   }
+
+  payload['token'] = token;
+
   console.log('Notification payload', JSON.stringify(payload));
 
   var docExists = false;
@@ -197,6 +82,8 @@ exports.sendPushNotification = functions.https.onRequest(async (req, res) => {
   }
 
   docData.totalCount = docData.totalCount + 1;
+
+  console.log('Sending payload', JSON.stringify(payload));
 
   var messageId;
   try {
