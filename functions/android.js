be9c2edef156e4a9fdc4f82fbc40fa247a091ee1
Basset, Justin <Bassett.JustinT@gmail.com>
2020-02-06 00:08:39 -0500

    Fix android notifs.

--- functions/android.js
+++ functions/android.js
@@ -6,23 +6,24 @@ module.exports = {
     };
     let updateRateLimits = true;
 
-    for (const key of ['android', 'data']) {
-      if(req.body.data[key]){
-        payload[key] = req.body.data[key]
+    if(req.body.data){
+      for (const key of ['android', 'data']) {
+        if(req.body.data[key]){
+          payload[key] = req.body.data[key]
+        }
       }
-    }
-
-    // Handle the web actions by changing them into a format the app can handle
-    // https://www.home-assistant.io/integrations/html5/#actions
-    if(req.body.data && req.body.data.actions) {
-      for (let i = 0; i < req.body.data.actions.length; i++) {
-        const action = req.body.data.actions.actions[i];
-        payload.data["action_"+i+"_key"] = action.action
-        payload.data["action_"+i+"_title"] = action.title
-      }
+  
+      // Handle the web actions by changing them into a format the app can handle
+      // https://www.home-assistant.io/integrations/html5/#actions
+      if(req.body.data.actions) {
+        for (let i = 0; i < req.body.data.actions.length; i++) {
+          const action = req.body.data.actions.actions[i];
+          payload.data["action_"+i+"_key"] = action.action
+          payload.data["action_"+i+"_title"] = action.title
+        }
+      }  
     }
     
-
     // Always put message, title, and image in data so that the application can handle creating
     // the notifications.  This allows us to safely create actionable/imaged notifications.
     if(req.body.message) {
