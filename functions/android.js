ef5f63a7a349a1c55601ce3c6b3c2b8350b52d76
Daniel Shokouhi <dshokouhi@gmail.com>
2020-11-10 07:45:00 -0800

    Add broadcast intent and volume level commands to command list (#27)
    * Add broadcast intent command to command list
    
    * Add command volume level

--- functions/android.js
+++ functions/android.js
@@ -60,7 +60,8 @@ module.exports = {
     if(req.body.message) {
       payload.data.message = req.body.message
       if(req.body.message in ['request_location_update', 'clear_notification', 'remove_channel',
-			     'command_dnd', 'command_ringer_mode']) {
+			     'command_dnd', 'command_ringer_mode', 'command_broadcast_intent',
+			     'command_volume_level']) {
         updateRateLimits = false
       }
     }
