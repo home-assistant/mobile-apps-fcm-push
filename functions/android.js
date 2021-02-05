521730e9c564666d04dc63a40e06ab4cc94b6d68
Daniel Shokouhi <dshokouhi@gmail.com>
2021-02-05 08:33:08 -0800

    Add command to launch webview (#37)

--- functions/android.js
+++ functions/android.js
@@ -62,7 +62,7 @@ module.exports = {
       if(req.body.message in ['request_location_update', 'clear_notification', 'remove_channel',
 			     'command_dnd', 'command_ringer_mode', 'command_broadcast_intent',
 			     'command_volume_level', 'command_bluetooth', 'command_high_accuracy_mode',
-           'command_activity']) {
+           'command_activity', 'command_webview']) {
         updateRateLimits = false
       }
     }
