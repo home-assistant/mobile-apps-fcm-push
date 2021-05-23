c58a92c908634d98ac3703a50f1b86bbdd3e89b1
Zac West <zacwest@gmail.com>
2021-05-22 21:00:22 -0700

    Add tests and VSCode setup

---
+++ functions/test/legacy.test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('legacy.js', () => {
+  describe('failing test', function() {
+    it('should succeed', function() {
+      assert.equal(false, true);
+    });
+  });
+});
