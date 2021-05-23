edd87cfc48763e3feadebe133ecbec49f8e90d50
Zac West <zacwest@gmail.com>
2021-05-22 22:16:59 -0700

    Add input/output test scaffolding

--- functions/test/legacy.test.js
+++ functions/test/legacy.test.js
@@ -1,9 +1,48 @@
+const fs = require('fs');
 var assert = require('assert');
 
-describe('legacy.js', () => {
-  describe('failing test', function() {
-    it('should succeed', function() {
-      assert.equal(true, true);
+describe('legacy.js', function () {
+    const legacy = require('../legacy.js');
+    
+    describe('input/output tests', function () {
+        const dir = './test/fixtures/legacy/';
+        fs.readdirSync(dir).forEach(file => {
+            describe(`${file}`, function() {
+                var input;
+                var expectedApns;
+                var expectedRateLimit;
+                
+                before(function (done) {
+                    fs.readFile(dir + file, 'utf8', (err, data) => {
+                        if (err) {
+                            done(err);
+                            return;
+                        }
+                        
+                        const json = JSON.parse(data)
+                        input = json["input"]
+                        expected = {
+                            "payload": {
+                                "apns": {
+                                    "headers": json["headers"],
+                                    "payload": json["payload"]
+                                }
+                            },
+                            "updateRateLimits": (json["rate_limit"] == undefined) ? true : json["rate_limit"]
+                        }
+                        done();
+                    })
+                })
+                
+                it('should match the expected', function() {
+                    let result = legacy.createPayload({"body": input});
+                    // removing things that aren't worth copy/pasting between test cases
+                    delete result["payload"]["android"];
+                    delete result["payload"]["notification"];
+                    delete result["payload"]["fcm_options"];
+                    assert.deepStrictEqual(result, expected);
+                });
+            });
+        });
     });
-  });
 });
