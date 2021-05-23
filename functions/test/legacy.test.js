4dacfff16532a56612552fc555f2b8670d5ce52c
Zac West <zacwest@gmail.com>
2021-05-22 21:18:14 -0700

    Passing tests

--- functions/test/legacy.test.js
+++ functions/test/legacy.test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('legacy.js', () => {
   describe('failing test', function() {
     it('should succeed', function() {
-      assert.equal(false, true);
+      assert.equal(true, true);
     });
   });
 });
