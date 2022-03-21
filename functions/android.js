7165a7785c6f6ddd53a7a4908cce8058f3978925
Daniel Shokouhi <dshokouhi@gmail.com>
2022-03-21 11:09:47 -0700

    Add package name to key list

--- functions/android.js
+++ functions/android.js
@@ -48,7 +48,8 @@ module.exports = {
         'visibility', 'notificationCount', 'lightSettings', 'image', 'timeout', 'importance', 
         'subject', 'group', 'icon_url', 'ledColor', 'vibrationPattern', 'persistent', 
         'chronometer', 'when', 'alert_once', 'intent_class_name', 'notification_icon',
-        'ble_advertise', 'ble_transmit', 'video', 'high_accuracy_update_interval'
+        'ble_advertise', 'ble_transmit', 'video', 'high_accuracy_update_interval',
+        'package_name'
       ]) {
         if (req.body.data[key]){
           payload.data[key] = String(req.body.data[key]);
