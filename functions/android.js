8aa11872dbbb8c9dc59f7205e9a369e585a8d0a9
Daniel Shokouhi <dshokouhi@gmail.com>
2022-06-30 19:14:27 -0700

    Add new parameters for android (#61)
    * Add new parameters for android
    
    * Lint
    
    * Update android.js

--- functions/android.js
+++ functions/android.js
@@ -45,11 +45,14 @@ module.exports = {
         'bodyLocKey', 'bodyLocArgs', 'titleLocKey', 'titleLocArgs', 'channel',
         'ticker', 'sticky', 'eventTime', 'localOnly', 'notificationPriority',
         'defaultSound', 'defaultVibrateTimings', 'defaultLightSettings', 'vibrateTimings',
-        'visibility', 'notificationCount', 'lightSettings', 'image', 'timeout', 'importance', 
-        'subject', 'group', 'icon_url', 'ledColor', 'vibrationPattern', 'persistent', 
+        'visibility', 'notificationCount', 'lightSettings', 'image', 'timeout', 'importance',
+        'subject', 'group', 'icon_url', 'ledColor', 'vibrationPattern', 'persistent',
         'chronometer', 'when', 'alert_once', 'intent_class_name', 'notification_icon',
         'ble_advertise', 'ble_transmit', 'video', 'high_accuracy_update_interval',
-        'package_name'
+        'package_name', 'tts_text', 'media_stream', 'command', 'intent_package_name',
+        'intent_action', 'intent_extras', 'media_command', 'media_package_name', 'intent_uri',
+        'intent_type'
+
       ]) {
         if (req.body.data[key]){
           payload.data[key] = String(req.body.data[key]);
