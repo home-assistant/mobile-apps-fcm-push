9161981c16ccb05d9f5e47223aee0c44ff93cb76
Daniel Shokouhi <dshokouhi@gmail.com>
2022-09-24 16:15:35 -0700

    Add screen brightness commands

--- functions/android.js
+++ functions/android.js
@@ -67,7 +67,8 @@ module.exports = {
       const messages_to_ignore = ['request_location_update', 'clear_notification', 'remove_channel', 'command_dnd', 
       'command_ringer_mode', 'command_broadcast_intent','command_volume_level', 'command_screen_on',
       'command_bluetooth', 'command_high_accuracy_mode', 'command_activity', 'command_webview', 'command_media',
-      'command_update_sensors', 'command_ble_transmitter', 'command_persistent_connection', 'command_stop_tts'];
+      'command_update_sensors', 'command_ble_transmitter', 'command_persistent_connection', 'command_stop_tts',
+      'command_auto_screen_brightness', 'command_screen_brightness_level'];
       if (messages_to_ignore.includes(req.body.message)) {
         updateRateLimits = false;
       }
